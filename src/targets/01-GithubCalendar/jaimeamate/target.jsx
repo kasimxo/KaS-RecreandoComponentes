da5dfaa1bb6f42c67f90f78f81e0a89fd4bb757b
Jaime Amate <146433774+jaimeamate@users.noreply.github.com>
2025-02-14 17:20:53 +0100

    01-jaimeamate (#11)

---
+++ src/targets/01-GithubCalendar/jaimeamate/target.jsx
@@ -0,0 +1,130 @@
+import { StyleSheet, View, Text, Button } from 'react-native';
+import GenerateData from '../data'
+
+export default function GithubCalendar() {
+    const DATA = GenerateData();
+    const ACT_MONTH = new Date().getMonth() + 1;
+    const MONTHS = getTwelveNextMonths(ACT_MONTH)
+    const MONTH_NAMES = ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'];
+    const DAYS = ['Mon', 'Wed', 'Fri']
+    const ANHOS = [2021, 2020, 2019]
+    const COLORS = ["#161b22", "#3b0a45", "#5e0b6e", "#7f0c97", "#a00dbf", "#c10ee8"]
+
+    return (
+        <View style={styles.wrapper}>
+            <View style={styles.container}>
+                <View style={{ display: 'flex', flexDirection: 'row'}}>
+                    <View style={styles.daysLegend}>
+                        <View style={{ display: 'grid', gridTemplateRows: 'repeat(7, 1fr)', marginRight: 5}}>
+                            {
+                                Object.keys(DAYS).map((d, i) => <Text style={{ color: 'white', textAlign: 'end', gridRow: (i * 2) + 2 }}>{DAYS[i]}</Text>)
+                            }
+                        </View>
+                    </View>
+                    <View style={{flex: 1}}>
+                        <View style={{ display: 'grid', gridTemplateColumns: 'repeat(13, 1fr)', marginBottom: 5}}>
+                            {MONTHS.map((month, i) => <Text style={{ color: 'white' }}>{MONTH_NAMES[month - 1]}</Text>)}
+                        </View>
+                        <View style={styles.containerCalendar}>
+                            {
+                                DATA.map((week, i) =>
+                                    <View style={styles.weekContainer}>
+                                        {week.map((d, i) => (
+                                            <View style={[{ backgroundColor: COLORS[d] }, styles.dayCell]}></View>
+                                        ))}
+                                    </View>
+                                )
+                            }
+                        </View>
+                    </View>
+                </View>
+                <View style={styles.infoContainer}>
+                    <Text style={{ color: '#9198A1' }}>Learn how we count contributions</Text>
+                    <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+                        <Text style={{ color: 'white' }}>Less</Text>
+                        {COLORS.map((color, i) => <View style={{ backgroundColor: color, width: 17, height: 17, borderRadius: 5, margin: 3 }}></View>)}
+                        <Text style={{ color: 'white' }}>More</Text>
+                    </View>
+                </View>
+            </View>
+            {/* <View style={{marginLeft: '5px',borderColor: 'mediumpurple', borderWidth: 1, borderRadius: 5, display: 'flex', flexDirection: 'column', justifyContent: 'start', padding: 5, height: 'max-content'}}>
+                    {
+                        ANHOS.map((year, i) => (
+                            <View style={{marginBottom: 5,width: '100px', borderRadius: 5}}>
+                                <Button title={year.toString()} color={COLORS[COLORS.length-3]} onPress={() => {}}/>
+                            </View>
+                        ))
+                    }
+            </View> */}
+        </View>
+    );
+}
+
+function getTwelveNextMonths(actMonth) {
+    let months = []
+    for (let index = 0; index < 13; index++) {
+        months.push((actMonth + index - 1) % 12 + 1)
+
+    }
+    return months
+}
+
+const styles = StyleSheet.create({
+    wrapper: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'start',
+        width: '100%',
+    },
+    container: {
+        borderColor: 'mediumpurple',
+        borderWidth: 1,
+        borderRadius: 5,
+        display: 'flex',
+        width: '100%',
+        padding: 20
+    },
+    daysLegend: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    containerCalendar: {
+        width: '100%',
+        color: 'white',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        // paddingHorizontal: 20
+    },
+    weekContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 5,
+        // alignItems: 'center',
+        // marginRight: 10,
+        color: 'white'
+    },
+    weekNumber: {
+        textAlign: 'center',
+        marginBottom: 5,
+        color: 'white'
+    },
+    dayCell: {
+        textAlign: 'center',
+        borderRadius: 5,
+        width: 18,
+        height: 18,
+        // margin: 4,
+        color: 'white'
+    },
+    infoContainer: {
+        marginTop: 10,
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    }
+});
