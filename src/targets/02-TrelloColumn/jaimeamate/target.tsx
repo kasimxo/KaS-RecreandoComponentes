ae70e4b70968596593739e83038b69e8396792b4
Jaime Amate <jaime_amate@roadtodata.com>
2025-02-18 16:22:57 +0100

    02-jaimeamate base component

---
+++ src/targets/02-TrelloColumn/jaimeamate/target.tsx
@@ -0,0 +1,99 @@
+// Imports
+import React, { useState } from 'react';
+import { View, Text, FlatList, Pressable, Image, TextInput, Button } from 'react-native';
+import { styles } from './styles';
+import GenerateData from "../data";
+
+// Load column data
+const DATA = GenerateData();
+
+// Type Item
+type ItemProps = { title: string, id: number };
+
+// Component TrelloColumn
+const TrelloColumn = () => {
+    // States
+    const [isHovered, setIsHovered] = useState(false) // Hover add button state
+    const [isAdding, setIsAdding] = useState(false) // Adding state
+    const [input, onChangeText] = useState('') // Input state
+    const [cards, setCards] = useState(DATA.cards) // Data list (cards) state
+
+    // Component Item
+    const Item = ({ title, id }: ItemProps) => (
+        <View style={styles.item}>
+            <Text style={styles.whiteText}>{title}</Text>
+            <Pressable onPress={() => deleteItem(id)}>
+                <Image
+                    style={styles.deleteItemButton}
+                    source={require('@assets/cancelar.png')}
+                />
+            </Pressable>
+        </View>
+    );
+
+    // Functions ([CRUD] - only Create and Delete)
+
+    // Function to delete an item from the column
+    // It deletes the item with the id passed as a parameter
+    function deleteItem(id: number): void {
+        setCards(cards.filter((_, index) => index !== id));
+    }
+    
+    // Function to add an item into the column
+    // It adds the item passed as a parameter to the column
+    function addItem(item: string): void {
+        if (item.trim() !== '') {
+            setCards([...cards, item]);
+            onChangeText('');
+        }
+        setIsAdding(false);
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text style={[styles.whiteText, { fontWeight: 'bold', marginBottom: 5, marginLeft: 5 }]}>{DATA.title}</Text>
+            <FlatList
+                data={cards}
+                renderItem={({ item, index }) => <Item title={item} id={index}></Item>}
+                keyExtractor={(item, index) => index.toString()}
+            />
+            {
+                isAdding ?
+                    <View>
+                        <TextInput
+                            style={styles.inputText}
+                            multiline={true}
+                            maxLength={150}
+                            onChangeText={onChangeText}
+                            placeholder="Introduce un título de tarjeta"
+                            placeholderTextColor="#b6b6b6"
+                            value={input}
+                        />
+                        <View style={{ flexDirection: 'row', alignItems: 'center', gap: 10 }}>
+                            <Pressable onPress={() => addItem(input.toString())} style={{ backgroundColor: '#579dff', padding: 4, paddingHorizontal: 10, borderRadius: 5 }} >
+                                <Text style={{ color: '#FFF' }}>Añadir tarjeta</Text>
+                            </Pressable>
+                            <Pressable onPress={() => setIsAdding(false)}>
+                                <Image
+                                    style={styles.deleteItemButton}
+                                    source={require('@assets/cancelar.png')}
+                                />
+                            </Pressable>
+                        </View>
+                    </View>
+                    :
+                    <Pressable
+                        onHoverIn={() => setIsHovered(true)}
+                        onHoverOut={() => setIsHovered(false)}
+                        style={[styles.button, isHovered && styles.buttonHovered, { marginTop: 5 }]}
+                        onPress={() => setIsAdding(true)}
+                    >
+                        <Text style={styles.whiteText}>+ Añade una tarjeta</Text>
+                    </Pressable>
+            }
+        </View>
+    );
+};
+
+
+export default TrelloColumn;
