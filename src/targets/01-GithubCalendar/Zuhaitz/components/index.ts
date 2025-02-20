5489e9fc8149a1cc645908a56d8e82b426aca023
Zuhaitz Martínez <32980658+Zuhaitz@users.noreply.github.com>
2025-02-20 18:51:36 +0100

    01-Zuhaitz (#19)
    * 01-Zuhaitz file cleaning
    
    * 01-Zuhaitz file cleaning
    
    * 01-Zuhaitz
    
    * 01-Zuhaitz
    
    * 01-Zuhaitz
    
    * 01-Zuhaitz
    
    * 01-Zuhaitz

---
+++ src/targets/01-GithubCalendar/Zuhaitz/components/index.ts
@@ -0,0 +1,7 @@
+import Calendar from "./Calendar";
+import DayCell from "./DayCell";
+import MonthList from "./MonthList";
+import DayList from "./DayList";
+import Footer from "./Footer";
+
+export { Calendar, DayCell, MonthList, DayList, Footer };
