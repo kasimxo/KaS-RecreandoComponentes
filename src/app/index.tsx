56ea739cd74a920141f869f7c6d580e23569de8c
Zuhaitz Martínez <zuhaitzmartinezjimenez@gmail.com>
2025-02-20 14:18:24 +0100

    01-Zuhaitz

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,22 +1,17 @@
-import { View, Text, StyleSheet, Linking, ScrollView } from "react-native";
-import TargetsComponent from "./../components/targetsComponent";
-import { styles } from "./../styles/styles";
-import TextCardComponent from "../components/textCardComponent";
-import GithubCalendar from "@targets/01-GithubCalendar/Zuhaitz/target";
+import {View, Text, StyleSheet, Linking, ScrollView} from 'react-native';
+import TargetsComponent from './../components/targetsComponent'
+import {styles} from './../styles/styles'
+import TextCardComponent from '../components/textCardComponent';
 
-export default function Index() {
-  return (
-    <ScrollView
-      style={{ paddingHorizontal: 25 }}
-      contentContainerStyle={styles.componentlist}
-    >
-      <TextCardComponent
-        title={"Bienvenido/a!"}
-        content={`Recreando Componentes es el lugar en el que podrás aprender y mejorar como Frontend developer con ReactNative! Da igual si eres Junior, Senior o estás empezando, el objetivo es superarse recreando algunos de los componentes más icónicos de Internet.`}
-      />
-      <TargetsComponent />
-    </ScrollView>
+export default function Index(){
 
-    // <GithubCalendar />
-  );
+    return(
+        <ScrollView style={{paddingHorizontal:25}}
+            contentContainerStyle={styles.componentlist}>
+            <TextCardComponent 
+                title={"Bienvenido/a!"}
+                content={`Recreando Componentes es el lugar en el que podrás aprender y mejorar como Frontend developer con ReactNative! Da igual si eres Junior, Senior o estás empezando, el objetivo es superarse recreando algunos de los componentes más icónicos de Internet.`}/>
+            <TargetsComponent />
+        </ScrollView>
+    )
 }
