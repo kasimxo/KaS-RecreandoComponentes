ed4dc17fe3503d23e93a6a493defe4be34388f75
Zuhaitz Martínez <zuhaitzmartinezjimenez@gmail.com>
2025-03-17 13:44:20 +0100

    06-Zuhaitz almost done maybe

--- src/targets/06-RollingCharacters/Zuhaitz/components/RollingText.tsx
+++ src/targets/06-RollingCharacters/Zuhaitz/components/RollingText.tsx
@@ -1,20 +1,27 @@
 import { useEffect, useRef, useState } from "react";
-import { Text } from "react-native";
+import { Animated, Image, StyleSheet, Text, View } from "react-native";
 
 type RollingTextProps = {
   text: string;
   duration: number;
+  accentColor?: string;
 };
 
 const chars =
   "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_-+=[]{}|;:,.<>?/~";
 
-const RollingText = ({ text, duration }: RollingTextProps) => {
+const RollingText = ({
+  text,
+  duration,
+  accentColor = "#7f5cff",
+}: RollingTextProps) => {
   const index = useRef(0);
+  const finalText = useRef("");
   const [animatedText, setAnimatedText] = useState("");
 
   useEffect(() => {
     index.current = 0;
+    disappear();
 
     const rollText = setInterval(() => {
       let dum = text.slice(0, index.current);
@@ -24,17 +31,72 @@ const RollingText = ({ text, duration }: RollingTextProps) => {
       }
 
       setAnimatedText(dum);
+      finalText.current = text.slice(0, index.current);
 
-      if (index.current >= text.length) return clearInterval(rollText);
+      if (index.current >= text.length) {
+        appear();
+        return clearInterval(rollText);
+      }
       index.current += 1;
     }, (duration * 1000) / text.length);
+
+    return () => clearInterval(rollText);
   }, [text, duration]);
 
+  // Animations
+  const opacity = useState(new Animated.Value(0))[0];
+
+  const appear = () => {
+    Animated.timing(opacity, {
+      toValue: 1,
+      duration: 200,
+      useNativeDriver: false,
+    }).start();
+  };
+
+  const disappear = () => {
+    Animated.timing(opacity, {
+      toValue: 0,
+      duration: 100,
+      useNativeDriver: false,
+    }).start();
+  };
+
   return (
-    <Text style={{ color: "white", fontSize: 30, textAlign: "center" }}>
-      {animatedText}
-    </Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Soy </Text>
+
+      <Text style={styles.text}>
+        <Text style={{ color: "gray" }}>{animatedText}</Text>
+        <Text style={[styles.main, { color: accentColor }]}>
+          {finalText.current}
+        </Text>
+      </Text>
+
+      <Animated.Image
+        source={require("@assets/controlar.png")}
+        style={[styles.icon, { opacity: opacity }]}
+        tintColor={accentColor}
+      />
+    </View>
   );
 };
 
 export default RollingText;
+
+const styles = StyleSheet.create({
+  container: { flexDirection: "row" },
+  text: {
+    color: "white",
+    fontSize: 30,
+  },
+  main: {
+    position: "absolute",
+    left: 0,
+    width: "100%",
+  },
+  icon: {
+    width: 40,
+    height: 40,
+  },
+});
--- src/targets/06-RollingCharacters/Zuhaitz/target.tsx
+++ src/targets/06-RollingCharacters/Zuhaitz/target.tsx
@@ -1,22 +1,21 @@
-import { useState } from "react";
-import { Pressable, Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { View } from "react-native";
 
-import RollingText from "./components/RollingText";
 import GenerateData from "../data";
+import RollingText from "./components/RollingText";
 
 const RollinCharacters = () => {
   const [text, setText] = useState(GenerateData());
-
   const generateNewText = () => setText(GenerateData());
 
+  useEffect(() => {
+    const newWordInterval = setInterval(generateNewText, 3000);
+    return () => clearInterval(newWordInterval);
+  }, []);
+
   return (
-    <View style={{ marginBottom: 20 }}>
-      <RollingText text={text} duration={1} />
-      <Pressable onPress={generateNewText} style={{ backgroundColor: "green" }}>
-        <Text style={{ color: "white", fontSize: 30, textAlign: "center" }}>
-          ME
-        </Text>
-      </Pressable>
+    <View style={{ marginBottom: 20, minWidth: 300, justifyContent: "center" }}>
+      <RollingText text={text} duration={0.8} />
     </View>
   );
 };
